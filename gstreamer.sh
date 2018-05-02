5d88f1b534e12198dc3857e43c7a020b70f8d013
mike mcginty <mach327@gmail.com>
2018-05-02 19:13:49 -0400

    .

---
+++ gstreamer.sh
@@ -0,0 +1,18 @@
+latency=$2
+if [[ "x$latency" == "x" ]]; then
+	latency=200
+fi
+file=$1
+if [[ "x$file" == "x" ]]; then
+	file=a.mp4
+fi
+host=togo:8301
+location=rtsp://$host/$file
+
+gst-launch-1.0 rtspsrc location="$location" latency=$latency ! rtph264depay ! avdec_h264 ! autovideosink
+#gst-launch-1.0 rtspsrc location=rtsp://togo:8301/a.mp4 latency=$latency ! rtph264depay ! avdec_h264 ! autovideosink
+#gst-launch-1.0 rtspsrc location=rtsp://togo:8301/a.mp4 latency=100 ! rtph264depay ! avdec_h264 ! autovideosink
+#gst-launch-1.0 rtspsrc location=rtsp://togo:8301/a.mp4 latency=0 ! rtph264depay ! avdec_h264 ! autovideosink
+
+
+#gst-launch-1.0 rtspsrc location=rtsp://togo:8301/a.mp4 ! queue ! tee name=t ! rtph264depay t. !  h264parse t. ! capsfilter caps="video/x-h264,width=1280,height=800,framerate=(fraction)25/1" t. !  avdec_h264 t. ! autovideosink t. ! queue !  rtpg726depay ! avdec_g726 !  audioconvert ! audioresample ! autoaudiosink
---
+++ quad_view.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+#gst-launch-1.0 rtspsrc location="$location" latency=$latency ! rtph264depay ! avdec_h264 ! autovideosink
+
+
+latency=100
+i1="rtsp://togo:8301/a720.mp4"
+i2="rtsp://togo:8301/a720.mp4"
+i3="rtsp://togo:8301/a720.mp4"
+i4="rtsp://togo:8301/a720.mp4"
+
+source_1="videotestsrc"
+tcptimeout=1000000
+timeouttoretrywithtcp=0
+rtspsrc="rtspsrc timeout=$timeouttoretrywithtcp tcp-timeout=$tcptimeout "
+source_1="$rtspsrc location="$i1" latency="$latency" ! rtph264depay ! avdec_h264"
+source_2="$rtspsrc location="$i2" latency="$latency" ! rtph264depay ! avdec_h264"
+source_3="$rtspsrc location="$i3" latency="$latency" ! rtph264depay ! avdec_h264"
+source_4="$rtspsrc location="$i4" latency="$latency" ! rtph264depay ! avdec_h264"
+source_2="videotestsrc"
+source_3="videotestsrc"
+source_4="videotestsrc"
+
+#source_1="v4l2src"
+#source_2="videotestsrc pattern="snow""
+#gst-launch-1.0 $source_1  ! autovideosink
+#exit
+
+echo Sources:
+echo $source_1
+echo $source_2
+echo $source_3
+echo $source_4
+
+source_x=1280
+source_y=720
+#source_x=640
+#source_y=360
+xpos=$(($source_x))
+ypos=$(($source_y))
+echo x:$xpos
+echo y:$ypos
+
+gst-launch-1.0 glvideomixer name=mix \
+  sink_0::xpos=0 sink_0::ypos=0 \
+  sink_1::xpos=$xpos sink_1::ypos=0 \
+  sink_2::xpos=0 sink_2::ypos=$ypos \
+  sink_3::xpos=$xpos sink_3::ypos=$ypos ! autovideosink sync=false \
+  $source_1 ! mix. \
+  $source_2 ! mix. \
+  $source_3 ! mix. \
+  $source_4 ! mix.
+
+#removed videoconvert before autovideosink, unsure what that does but seems to still work with it removed
+
+#gst-launch-1.0 videomixer name=mix \
+  #sink_0::xpos=0 sink_0::ypos=0 \
+  #sink_1::xpos=960 sink_1::ypos=0 \
+  #sink_2::xpos=0 sink_2::ypos=540 \
+  #sink_3::xpos=960 sink_3::ypos=540 ! videoconvert ! autovideosink sync=false \
+  #videotestsrc ! mix. \
+  #videotestsrc ! mix. \
+  #videotestsrc ! mix. \
+  #videotestsrc ! mix.
+
+#gst-launch-1.0 videomixer name=mix \
+  #sink_0::xpos=0 sink_0::ypos=0 \
+  #sink_1::xpos=960 sink_1::ypos=0 \
+  #sink_2::xpos=0 sink_2::ypos=540 \
+  #sink_3::xpos=960 sink_3::ypos=540 ! videoconvert ! autovideosink sync=false \
+  #$source_1 ! rtph264depay ! avdec_h264 ! mix. \
+  #$source_2 ! rtph264depay ! avdec_h264 ! mix. \
+  #$source_3 ! rtph264depay ! avdec_h264 ! mix. \
+  #$source_4 ! rtph264depay ! avdec_h264 ! mix.
